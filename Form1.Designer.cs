bd708917ea84c040736b0b46926781359a1bac8a
hgu0524 <hgu0524@gmail.com>
2026-03-19 14:47:48 +0900

    과제1 코딩과 테스트 완료 TextBox 입력값을 ListBox에 추가하는 기능 구현 전송 버튼 클릭 시 메시지 출력 입력 후 TextBox 초기화 기능 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            title = new Label();
+            textmessage = new TextBox();
+            btnsend = new Button();
+            listBoxchat = new ListBox();
             SuspendLayout();
+            // 
+            // title
+            // 
+            title.Font = new Font("Segoe Script", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            title.Location = new Point(36, 9);
+            title.Name = "title";
+            title.Size = new Size(527, 95);
+            title.TabIndex = 0;
+            title.Text = "Echo Messenger";
+            // 
+            // textmessage
+            // 
+            textmessage.Location = new Point(40, 422);
+            textmessage.Name = "textmessage";
+            textmessage.Size = new Size(614, 23);
+            textmessage.TabIndex = 1;
+            textmessage.TextChanged += textBox1_TextChanged;
+            // 
+            // btnsend
+            // 
+            btnsend.BackColor = SystemColors.MenuHighlight;
+            btnsend.Font = new Font("맑은 고딕", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnsend.Location = new Point(676, 403);
+            btnsend.Name = "btnsend";
+            btnsend.Size = new Size(128, 60);
+            btnsend.TabIndex = 2;
+            btnsend.Text = "전송";
+            btnsend.UseVisualStyleBackColor = false;
+            btnsend.Click += btnsend_Click;
+            // 
+            // listBoxchat
+            // 
+            listBoxchat.Font = new Font("맑은 고딕", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            listBoxchat.FormattingEnabled = true;
+            listBoxchat.Location = new Point(40, 93);
+            listBoxchat.Name = "listBoxchat";
+            listBoxchat.Size = new Size(764, 304);
+            listBoxchat.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
             ClientSize = new Size(919, 521);
+            Controls.Add(listBoxchat);
+            Controls.Add(btnsend);
+            Controls.Add(textmessage);
+            Controls.Add(title);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label title;
+        private TextBox textmessage;
+        private Button btnsend;
+        private ListBox listBoxchat;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,22 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            // 1. 입력값 가져오기
+            string msg = textmessage.Text;
+
+            // 2. 리스트박스에 추가
+            listBoxchat.Items.Add(msg);
+
+            // 3. 입력창 비우기
+            textmessage.Clear();
+        }
     }
 }
